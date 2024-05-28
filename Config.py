b0ce51a6a80c34902681035797395eac08eef3a9
SakirBey1 <sakirhack81@gmail.com>
2024-05-28 17:55:31 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,13 +4,13 @@ import os
 class Config():
     # Get these values from my.telegram.org
     # https://my.telegram.org
-    API_ID = int(os.environ.get("3793309"))
-    API_HASH = os.environ.get("fe42ab8e509ea3f4628e82de05f3f1e3")
-    BOT_TOKEN = os.environ.get("5125459394:AAHQaPvKN66HJJlsnQIB5B-c4r-BouJzmd0")
-    BOT_USERNAME = os.environ.get("KaptanTaggerBot")
-    BOT_NAME = os.environ.get("Kaptan Tagger")
+    API_ID = int(os.environ.get("25512251"))
+    API_HASH = os.environ.get("86cf5ff56e56c4f20a7b1295b130e720")
+    BOT_TOKEN = os.environ.get("6918123071:AAGgqOLJuM4kLbkBH7bMjq3ycJK9PJl2sQE")
+    BOT_USERNAME = os.environ.get("sessiztagger_bot")
+    BOT_NAME = os.environ.get("Sessiz Tagger")
     BOT_ID = int(os.environ.get("5125459394"))
-    SUDO_USERS = os.environ.get("1948748468").split()
-    SUPPORT_CHAT = os.environ.get("KaptanSupportGroup")
-    OWNER_ID = int(os.environ.get("1948748468"))
-    OWNER_USERNAME = os.environ.get("SakirBey1")
+    SUDO_USERS = os.environ.get("5710250764").split()
+    SUPPORT_CHAT = os.environ.get("olimposchat")
+    OWNER_ID = int(os.environ.get("5710250764"))
+    OWNER_USERNAME = os.environ.get("SakirBey")
